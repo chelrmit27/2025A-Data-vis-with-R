3453ae3bab6e3a5ca128eae0dccb8ed27660e8af
Rachel <s3999159@rmit.edu.vn>
2025-05-23 11:57:36 +0700

    quarto touchup

--- .netlify/install_quarto.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Download and install Quarto
-curl -L -o quarto.deb https://github.com/quarto-dev/quarto-cli/releases/download/v1.4.550/quarto-1.4.550-linux-amd64.deb
-sudo dpkg -i quarto.deb
